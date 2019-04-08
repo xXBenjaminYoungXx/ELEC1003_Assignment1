9b8e8beca1c3fdc8442c9a701d58c9ba8c360b4e
xXBenjaminYoungXx <BenjaminYoung7198@gmail.com>
2019-04-09 09:56:26 +1000

    Added encripkey function

--- main.c
+++ main.c
@@ -18,28 +18,36 @@ to execute code required to encript/decript information.
 
 //----------------------------------------------------------------------------------------------------------------------------------
 
-void encripRotation();
+void encripRotation(char String1[], int key);
+void decripRotation(char String1[], int key);
 
 
 //----------------------------------------------------------------------------------------------------------------------------------
-
+//TODO: scanf is currently being used as a debugger
+//TODO: hard code file I/O, usese # to symbolise key
 int main(){
+    int menuChoice = 1;//This will be used to store users menu choice
+    int key = 2;
+    char String1[100];
+    //scanf("%d\n", &menuChoice);
+    //scanf("%d\n", &key);
+    scanf("%[^\n]s", &String1);
 
-    int menuChoice = 0;//This will be used to store users menu choice
-
-    //TODO: scanf is currently being used as a debugger
-    //TODO: hard code file I/O, usese # to symbolise key
-    scanf("%d", &menuChoice);
+    //Following changes strings to uppercase
+    for(int count = 0; count<100; count++){
+        if(String1[count]>=97&&String1[count]<=122){
+            String1[count] = String1[count] - 32;
+        }
+    }
 
     //Switch Statement
     switch(menuChoice){
 
         //Encryption of a message with a rotation cipher given the message text and rotation amoun
-        case 1: encripRotation();
+        case 1: encripRotation(String1, key);
             break;
         //Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
-        case 2:
-
+        case 2: decripRotation(String1, key);
             break;
         case 3://Encryption of a message with a substitution cipher given message text and alphabet substitution
 
@@ -67,14 +75,26 @@ This function executes code that takes static string input and encripts using cy
 
 */
 
-void encripRotation(char String1[]){
-    int cypherKey = 0;
-    char String1[100];
-
-    scanf("%d\n", &cypherKey);
-    scanf("%[^\n]s", &String1);
-
-
+void encripRotation(char String1[], int key){
+    //E(m) = (m+k)mod26
+    for(int count = 0; count<100; count++){
+        if(String1[count]>=65&&String1[count]<=90){
+            String1[count] = 65+(((String1[count])-65)+key)%26;
+        }
+    }
+    printf("%s", String1);
 }
+//----------------------------------------------------------------------------------------------------------------------------------
+/**
+This function executes code that takes static string input and decripts using cypher key.
 
-
+*/
+void decripRotation(char String1[], int key){
+    //E(m) = (m+k)mod26
+    for(int count = 0; count<100; count++){
+        if(String1[count]>=65&&String1[count]<=90){
+            String1[count] = 65+(((String1[count])-65)-key)%26;
+        }
+    }
+    printf("%s", String1);
+}
