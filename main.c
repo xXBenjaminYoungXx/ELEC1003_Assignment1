acd5085d435744a282ceb13edf80ca29a775b1bc
xXBenjaminYoungXx <BenjaminYoung7198@gmail.com>
2019-04-09 10:33:56 +1000

    Update main.c

--- main.c
+++ main.c
@@ -20,21 +20,27 @@ to execute code required to encript/decript information.
 
 void encripRotation(char String1[], int key);
 void decripRotation(char String1[], int key);
+void encriptSubstitution(char String1[], char sub[]);
+
 
 
 //----------------------------------------------------------------------------------------------------------------------------------
 //TODO: scanf is currently being used as a debugger
 //TODO: hard code file I/O, usese # to symbolise key
 int main(){
-    int menuChoice = 2;//This will be used to store users menu choice
+    //Declare file pointers
+
+    int menuChoice = 3;//This will be used to store users menu choice
     int key = 20;
-    char String1[100];
+    char String1[10000];
+    char Sub[26];
     //scanf("%d\n", &menuChoice);
     //scanf("%d\n", &key);
     scanf("%[^\n]s", &String1);
 
+
     //Following changes strings to uppercase
-    for(int count = 0; count<100; count++){
+    for(int count = 0; count<10000; count++){
         if(String1[count]>=97&&String1[count]<=122){
             String1[count] = String1[count] - 32;
         }
@@ -49,8 +55,8 @@ int main(){
         //Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
         case 2: decripRotation(String1, key);
             break;
-        case 3://Encryption of a message with a substitution cipher given message text and alphabet substitution
-
+        //Encryption of a message with a substitution cipher given message text and alphabet substitution
+        case 3: encriptSubstitution(String1, Sub);
             break;
         case 4://Decryption of a message encrypted with a substitution cipher given cipher text and substitutions
 
@@ -77,7 +83,7 @@ This function executes code that takes static string input and encripts using cy
 
 void encripRotation(char String1[], int key){
     //E(m) = (m+k)mod26
-    for(int count = 0; count<100; count++){
+    for(int count = 0; count<10000; count++){
         if(String1[count]>=65&&String1[count]<=90){
             String1[count] = 65+(((String1[count])-65)+key)%26;
         }
@@ -92,7 +98,7 @@ This function executes code that takes static string input and decripts using cy
 void decripRotation(char String1[], int key){
     //E(m) = (m+k)mod26
     int add = 0;
-    for(int count = 0; count<100; count++){
+    for(int count = 0; count<10000; count++){
         if((String1[count]-65)-key<0){
             add = 26;
         }
@@ -100,6 +106,72 @@ void decripRotation(char String1[], int key){
             String1[count] = 65+(((String1[count])-65)-key)%26 + add;
         }
         add = 0;
+    }
+    printf("%s", String1);
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+/**
+This function executes code that takes static string input and encrips using substitution string.
+
+*/
+void encriptSubstitution(char String1[], char sub[]){
+
+    for(int count = 0; count<10000; count++){
+        switch (String1[count]-64){
+        case 1: String1[count]=sub[0];
+            break;
+        case 2: String1[count]=sub[1];
+            break;
+        case 3: String1[count]=sub[2];
+            break;
+        case 4: String1[count]=sub[3];
+            break;
+        case 5: String1[count]=sub[4];
+            break;
+        case 6: String1[count]=sub[5];
+            break;
+        case 7: String1[count]=sub[6];
+            break;
+        case 8: String1[count]=sub[7];
+            break;
+        case 9: String1[count]=sub[8];
+            break;
+        case 10: String1[count]=sub[9];
+            break;
+        case 11: String1[count]=sub[10];
+            break;
+        case 12: String1[count]=sub[11];
+            break;
+        case 13: String1[count]=sub[12];
+            break;
+        case 14: String1[count]=sub[13];
+            break;
+        case 15: String1[count]=sub[14];
+            break;
+        case 16: String1[count]=sub[15];
+            break;
+        case 17: String1[count]=sub[16];
+            break;
+        case 18: String1[count]=sub[17];
+            break;
+        case 19: String1[count]=sub[18];
+            break;
+        case 20: String1[count]=sub[19];
+            break;
+        case 21: String1[count]=sub[20];
+            break;
+        case 22: String1[count]=sub[21];
+            break;
+        case 23: String1[count]=sub[22];
+            break;
+        case 24: String1[count]=sub[23];
+            break;
+        case 25: String1[count]=sub[24];
+            break;
+        case 26: String1[count]=sub[25];
+            break;
+        default: break;
+        }
     }
     printf("%s", String1);
 }
